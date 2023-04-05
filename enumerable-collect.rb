eb72e19892ea109f1d2dc3f45b14eeab8bd26038
Nirupma-Verma <113432409+Nirupma-Verma@users.noreply.github.com>
2023-04-05 14:30:25 +0530

    Create enumerable-collect.rb

---
+++ enumerable-collect.rb
@@ -0,0 +1,20 @@
+#write a method which takes an array of strings (containing secret enemy message bits!) and 
+#decodes its elements using ROT13 cipher system; returning an array containing the final messages.
+
+words = 'Hello, world!'
+
+words.tr('A-Za-z', 'N-ZA-Mn-za-m') #shortcut method 
+
+capltr = ('A'..'Z').to_a
+smalltr = ('a'..'z').to_a
+
+words.chars.each_with_index do |char, index|
+  next unless capltr.include?(char) or smalltr.include?(char)    
+  if capltr.include?(char)
+    words[index] = capltr[capltr.index(char) - 13]
+  else
+    words[index] = smalltr[smalltr.index(char) - 13]
+  end
+end
+
+puts words
